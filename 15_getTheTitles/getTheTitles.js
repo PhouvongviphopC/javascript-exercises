a0736ef44f01ceb6bee1214c38f46af793d6960b
PhouvongviphopC <phouvongviphopc@gmail.com>
2025-06-11 17:38:47 +0700

    update

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //the map method to extract the title from each book object
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const oldestPerson = people.reduce((oldest, person) => {
+        const birthYear = person.yearOfBirth || 0;
+        const deathYear = person.yearOfDeath || currentYear;
+        const age = deathYear - birthYear;
 
+        if (!oldest || age > oldest.age) {
+            return { name: person.name, age: age };
+        }
+        return oldest;
+    }, null);
+    return oldestPerson;
 };
 
 // Do not edit below this line
